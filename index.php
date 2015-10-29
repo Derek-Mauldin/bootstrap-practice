15befa821501c1d29ada1406dde6c7fd4c5b3002
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-10-29 17:00:17 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE htmL>
+	<html lang="en">
+
+		<head>
+			<meta charset="UTF-8" />
+			<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+			<!-- Bootstrap CSS -->
+			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+
+			<!-- Optional Theme -->
+			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.32/css/bootstrap-theme.min.css" />
+
+			<!-- HTML 5 shiv and Respond.js for IE8 support of HTML 5 elements and media queries -->
+			<!-- WARNING: Respond.js doesn't work if you view the page via fiel:// -->
+			<!-- [if lt IE 9]>
+				<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+				<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<! [endif] -->
+
+
+
+		</head>
+
+
+</html>
