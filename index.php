43aaecf27df0915b487a5bd9225e644917d92e65
Derek Mauildin <derek.e.mauldin@gmail.com>
2015-11-02 10:17:13 -0700

    added index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	<html lang="en">
 
 		<head>
+
 			<meta charset="UTF-8" />
 			<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -12,6 +13,9 @@
 			<!-- Optional Theme -->
 			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.32/css/bootstrap-theme.min.css" />
 
+			<!-- my stylesheet -->
+			<link rel="stylesheet" href="css/practice.css"
+
 			<!-- HTML 5 shiv and Respond.js for IE8 support of HTML 5 elements and media queries -->
 			<!-- WARNING: Respond.js doesn't work if you view the page via fiel:// -->
 			<!-- [if lt IE 9]>
@@ -19,9 +23,73 @@
 				<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 			<! [endif] -->
 
+			<!-- jQuery (necessary for Bootstrap's JavaScrip plugins -->
+			<script> src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'</script>
 
+			<!-- Include all compiled plugins (below)or include individual files as needed -->
+			<script src="js/bootstrap.min.js"></script>
+
+			<!-- jQuery (necessary for Bootstrap's JavaScrip plugins -->
+			<script> src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'</script>
+
+			<!-- Include all compiled plugins (below)or include individual files as needed -->
+			<script src="js/bootstrap.min.js"></script>
+
+			<title>Bootstrap Practice</title>
 
 		</head>
+
+		<body>
+
+			<header class="container">
+
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8">
+							<h1>Derek Mauldin</h1>
+						</div>
+						<div class="col-md-4">
+							<h2>Links</h2>
+						</div>
+					</div>
+				</div>
+
+			</header>
+
+				<main>
+
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<p>content</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3">
+								<p>content</p>
+							</div>
+							<div class="col-md-3">
+								<p>content</p>
+							</div>
+							<div class="col-md-3">
+								<p>content</p>
+							</div>
+							<div class="col-md-3">
+								<p>content</p>
+							</div>
+						</div>
+					</div>
+
+
+				</main>
+
+
+
+
+		</body>
 
 
 </html>
